438c2548d62c3e791b1ee430b218c55ed3d46ab1
abdullahal5 <abdullahalfahin183@gmail.com>
2023-10-18 17:26:25 -0700

    find operation complete

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const cors = require('cors')
 const app = express()
 const port = process.env.PORT || 4000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 
 app.use(cors())
@@ -43,11 +43,14 @@ async function run() {
       const result = await cursor.toArray()
       res.send(result)
     })
-    app.delete('/car/:id', async(req, res) =>{
-      
+    app.get('/car/:id', async(req, res) =>{
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id)}
+      const result = await carCollection.findOne(query)
+      res.send(result)
     })
 
-
+ 
 
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
